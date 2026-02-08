952d565a4df556dc1743403377fc89873d1f3d63
Alex Wiegand <wiegand.alex@gmail.com>
2026-02-07 19:36:12 -0500

    fix mobile menu styling

--- src/lib/components/website-components/MobileMenu.svelte
+++ src/lib/components/website-components/MobileMenu.svelte
@@ -3,9 +3,9 @@
     import * as Drawer from "$lib/components/ui/drawer/index.js";
     import { navigationLinks } from "$lib/helpers/navigation";
     // Common styling for consistency between links and triggers
-    const navItemStyle = "text-lg bg-(none) mx-2";
+    const navItemStyle = "text-lg bg-(none) mx-2 px-4";
     const menuItemStyle = "text-(--bbtb-foreground)"
-    const mobileMenuStyle = "bg-(--bbtb-background) p-4 shadow-lg rounded-md";
+    const mobileMenuStyle = "bg-(--bbtb-background) m-2 p-4 shadow-lg rounded-md";
     // State for Drawer (Mobile Menu)
     let open = $state(false)
     function handleOpenChange() {
@@ -13,17 +13,23 @@
     }
 </script>
 
-
+<div class="lg:hidden w-full h-10 bg-(--bbtb-foreground)"></div>
 <Drawer.Root bind:open>
-    <Drawer.Trigger class="lg:hidden w-full h-10 bg-(--bbtb-foreground) text-(--bbtb-background)">Menu</Drawer.Trigger>
+    <Drawer.Trigger class="lg:hidden fixed top-4 right-4 rounded-md shadow-sm shadow-black w-10 h-10 bg-(--bbtb-foreground) p-2">
+        <div class="h-6 w-6 flex flex-col justify-around">
+            <div class="h-1 w-full bg-(--bbtb-background) rounded-sm"></div>
+            <div class="h-1 w-full bg-(--bbtb-background) rounded-sm"></div>
+            <div class="h-1 w-full bg-(--bbtb-background) rounded-sm"></div>
+        </div>
+    </Drawer.Trigger>
     <Drawer.Content class='bg-(--bbtb-foreground) text-(--bbtb-background)'>
       <Drawer.Header>
         <Drawer.Title class='text-center text-lg underline text-(--bbtb-background)'>Navigation</Drawer.Title>
       </Drawer.Header>
-      <NavigationMenu.List class=" w-full flex-col items-start bg-(--bbtb-foreground) text-(--bbtb-background)">
+      <NavigationMenu.List class="w-full flex-col items-start bg-(--bbtb-foreground) text-(--bbtb-background)">
         {#each navigationLinks as link}
           {#if link.submenu}
-            <NavigationMenu.Item class="flex">
+            <NavigationMenu.Item>
               <NavigationMenu.Trigger class={navItemStyle}>
                 {link.title}
               </NavigationMenu.Trigger>
